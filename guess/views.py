1d5b5a971178689576820195698c8030b266d918
Julia Zeng <juliazengstl@gmail.com>
2020-07-17 11:20:20 -0500

    add game over screen

--- guess/views.py
+++ guess/views.py
@@ -16,7 +16,7 @@ def play(request):
         request.session["livesChange"] = 0
     if request.method == "POST":
         answer = request.POST.get('answer')
-        correct = randint(1, 31)
+        correct = randint(1, 30)
         offset = abs(int(answer)-correct)
         request.session["score"] += 30-offset
         request.session["scoreChange"] = 30-offset
@@ -28,6 +28,21 @@ def play(request):
             request.session["livesChange"] = 1
         else:
             request.session["livesChange"] = 0
-    return render(request, "guess/play.html", {
-        "score":request.session["score"], "scoreChange":request.session["scoreChange"], "lives":request.session["lives"], "livesChange":request.session["livesChange"], "answer":answer, "correct":correct
-    })
+    if request.session["lives"] < 1:
+        score = request.session["score"]
+        scoreChange = request.session["scoreChange"]
+        finalAnswer = answer
+        finalCorrect = correct
+        request.session["score"] = 0
+        request.session["lives"] = 10
+        request.session["scoreChange"] = 0
+        request.session["livesChange"] = 0
+        answer = -1
+        correct = -1
+        return render(request, "guess/end.html", {
+            "score":score, "scoreChange":scoreChange, "finalAnswer":finalAnswer, "finalCorrect":finalCorrect
+        })
+    else:
+        return render(request, "guess/play.html", {
+            "score":request.session["score"], "scoreChange":request.session["scoreChange"], "lives":request.session["lives"], "livesChange":request.session["livesChange"], "answer":answer, "correct":correct
+        })
